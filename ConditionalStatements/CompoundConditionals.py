b346f62b84b2a64c3a2c29277683bda88148c892
jeyaragu <jeyaragus@gmail.com>
2025-08-28 01:44:13 +0530

    added vowels or consonants and exam result programs

--- ConditionalStatements/CompoundConditionals.py
+++ ConditionalStatements/CompoundConditionals.py
@@ -1,15 +1,15 @@
 # Finding Odd or even number
-n = 18
-moduleN = n % 2
-if(moduleN > 0):
-    print('Weird')
-else:
-    if(n >= 2 and n <=5):
-        print('Not Weird')
-    elif(n >= 6 and n <=20 ):
-        print('Weird')
-    else:
-        print('Not Weird')
+# n = 18
+# moduleN = n % 2
+# if(moduleN > 0):
+#     print('Weird')
+# else:
+#     if(n >= 2 and n <=5):
+#         print('Not Weird')
+#     elif(n >= 6 and n <=20 ):
+#         print('Weird')
+#     else:
+#         print('Not Weird')
 
 # Finding Vote eligibility
 # age = int(input('Enter age:'))
@@ -26,15 +26,32 @@ else:
 #     print('Not Eligible for work ' + str(inputAge))
 
 # Valid Marks
-validMark = int(input('Enter the mark: '))
-if validMark > 0 and validMark <= 100:
-    print('Valid Mark ' + str(validMark))
-else:
-    print('Invalid Mark ' + str(validMark))    
+# validMark = int(input('Enter the mark: '))
+# if validMark > 0 and validMark <= 100:
+#     print('Valid Mark ' + str(validMark))
+# else:
+#     print('Invalid Mark ' + str(validMark))    
 
 # Gender Check
-genderInput = input('Enter M or F: ')
-if genderInput == 'M' or genderInput == 'm':
-    print('Gender is Male')
+# genderInput = input('Enter M or F: ')
+# if genderInput == 'M' or genderInput == 'm':
+#     print('Gender is Male')
+# else:
+#     print('Gender is female')
+
+# Vowels or consonants
+# letter = input('Enter alphapets in lower case: ')
+# if letter == 'a' or letter == 'e' or letter =='i' or letter =='o' or letter =='u':
+#     print(letter, 'is a vowles')
+# else:
+#     print(letter, 'is a consonant') 
+
+# Exam Result
+maths = input('Enter the Maths Marks: ')
+physics = input('Enter the Physics Marks: ')
+chemistry = input('Enter the Chemistry Marks: ')
+miniMumPassMark = 45
+if int(maths) >= miniMumPassMark and int(physics) >= miniMumPassMark and int(chemistry) >= miniMumPassMark:
+    print('Pass')
 else:
-    print('Gender is female') 
+    print('Fail')
